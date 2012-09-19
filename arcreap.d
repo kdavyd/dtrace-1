7f0211bb11808d41b31967278736f6785d92034e
kdavyd <kdavyd@gmail.com>
2012-09-19 17:01:05 -0500

    Update arcreap.d

--- arcreap.d
+++ arcreap.d
@@ -8,23 +8,17 @@ fbt::arc_kmem_reap_now:entry
 fbt::arc_adjust:entry
 {
     self->start[probefunc] = timestamp;
-    self->inadjust = 1;
-}
-
-fbt::arc_evict:entry
-/self->inadjust/
-{
-    self->start[probefunc] = timestamp;
 }
 
 fbt::arc_shrink:entry
 {
+    self->start[probefunc] = timestamp;
     trace("called");
 }
 
 fbt::arc_kmem_reap_now:return,
 fbt::arc_adjust:return,
-fbt::arc_evict:return
+fbt::arc_shrink:return
 /self->start[probefunc]/
 {
     printf("%Y %d ms", walltimestamp,
